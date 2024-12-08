62daa600d682b67a29cb40dd39f54d7265703b4a
«bezgolovii» <aleksandrtataurov@gmail.com>
2024-12-08 19:16:27 +0800

    solving the problem

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,4 +3,23 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
+string[] inputArray = new string[] { "aa", "bbbb", "c", "dddd", "e", "ff", "ggg", "hhhh", "iii", "jjj" };
 
+string[] shortStrings = new string[0];
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3)
+    {
+        string[] temp = new string[shortStrings.Length + 1];
+        Array.Copy(shortStrings, temp, shortStrings.Length);
+        temp[shortStrings.Length] = inputArray[i];
+        shortStrings = temp;
+    }
+}
+
+
+foreach (string s in shortStrings)
+{
+    Console.Write(s + " , ");
+}
--- task1/obj/Debug/net9.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net9.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c3620bc8ca5ca5ed0fb879f30edd0e8731b93d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
