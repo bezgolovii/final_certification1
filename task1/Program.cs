6d9e6457d708ea89cdaa55d8f48470aa7e61faf9
«bezgolovii» <aleksandrtataurov@gmail.com>
2024-12-09 11:52:08 +0800

    change_of_decision

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,22 +3,46 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
-string[] inputArray = new string[] { "aa", "bbbb", "c", "dddd", "e", "ff", "ggg", "hhhh", "iii", "jjj" };
+// string[] inputArray = new string[] { "aa", "bbbb", "c", "dddd", "e", "ff", "ggg", "hhhh", "iii", "jjj" };
 
-string[] shortStrings = new string[0];
+// string[] shortStrings = new string[0];
 
-for (int i = 0; i < inputArray.Length; i++)
+// for (int i = 0; i < inputArray.Length; i++)
+// {
+//     if (inputArray[i].Length <= 3)
+//     {
+//         string[] temp = new string[shortStrings.Length + 1];
+//         Array.Copy(shortStrings, temp, shortStrings.Length);
+//         temp[shortStrings.Length] = inputArray[i];
+//         shortStrings = temp;
+//     }
+// }
+
+// foreach (string s in shortStrings)
+// {
+//     Console.Write(s + " , ");
+// }
+
+string[] array1 = new string[6] {"257", "45", "hello", "world", "res","456"};
+string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
 {
-    if (inputArray[i].Length <= 3)
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
     {
-        string[] temp = new string[shortStrings.Length + 1];
-        Array.Copy(shortStrings, temp, shortStrings.Length);
-        temp[shortStrings.Length] = inputArray[i];
-        shortStrings = temp;
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
     }
-}
-
-foreach (string s in shortStrings)
+}void PrintArray(string[] array)
 {
-    Console.Write(s + " , ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
+SecondArray(array1, array2);
+PrintArray(array2);
