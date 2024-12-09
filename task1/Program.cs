ecfa40096eb1bfe16c23f96084b72bc30852a700
«bezgolovii» <aleksandrtataurov@gmail.com>
2024-12-09 11:55:02 +0800

    deleting_version_1

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,26 +3,6 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
-// string[] inputArray = new string[] { "aa", "bbbb", "c", "dddd", "e", "ff", "ggg", "hhhh", "iii", "jjj" };
-
-// string[] shortStrings = new string[0];
-
-// for (int i = 0; i < inputArray.Length; i++)
-// {
-//     if (inputArray[i].Length <= 3)
-//     {
-//         string[] temp = new string[shortStrings.Length + 1];
-//         Array.Copy(shortStrings, temp, shortStrings.Length);
-//         temp[shortStrings.Length] = inputArray[i];
-//         shortStrings = temp;
-//     }
-// }
-
-// foreach (string s in shortStrings)
-// {
-//     Console.Write(s + " , ");
-// }
-
 string[] array1 = new string[6] {"257", "45", "hello", "world", "res","456"};
 string[] array2 = new string[array1.Length];
 void SecondArray(string[] array1, string[] array2)
